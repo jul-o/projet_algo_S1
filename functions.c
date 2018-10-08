4af7c7602eab0661054099af2ad70bb8d4ad13d6
Jules Sang <sangj@ensipc688.ensimag.fr>
2018-10-08 13:49:48 +0200

    debut correction erreurs

--- functions.c
+++ functions.c
@@ -19,14 +19,24 @@ struct tiling* loadTiling(char * filePath){
   // The temp is there to read all lines to simplify code later (temp value should be null)
   int lines, columns;
   char * temp = NULL;
+  // TODO : utiliser une autre méthode pour interdire les mauvais caractères
   fscanf(f, "%d %d %s", &lines, &columns, temp);
+
+  if(lines <= 0){
+    printf("le nombre de lignes doit être supérieur à 0 \n");
+    return NULL;
+  }
+  if(columns <= 0){
+    printf("le nombre de colonnes doit être supérieur à 0 %d\n", columns);
+    return NULL;
+  }
 
   // bool tiling [lines][columns];
   bool** tiling = malloc(sizeof(int*)*columns);
   for(int i = 0; i < columns; i++){
     tiling[i] = malloc(sizeof(int)*lines);
   }
-  // memset(tiling, false, sizeof(tiling[0][0]) * lines * columns); Not usefule to have a default value ?
+  // memset(tiling, false, sizeof(tiling[0][0]) * lines * columns); Not useful to have a default value ?
 
   char buffer[MAX_SIZE_LINE] = "";
   int current;
@@ -39,6 +49,11 @@ struct tiling* loadTiling(char * filePath){
       current = buffer[i] - '0'; // Simple trick to convert the char to int
       tiling[currentLine][currentColumn] = current;
       currentColumn++;
+    }
+    //vérification de la taille de la ligne
+    if(currentColumn != columns){
+      printf("Erreur : ligne de mauvaise taille (ligne %d)", currentLine);
+      return NULL;
     }
     currentLine++;
   }
