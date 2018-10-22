ab7b75d0aa8ce3bc24456f8760db5e7292f4ca36
Hilderic <hilderic-sauret@live.fr>
2018-10-22 19:31:04 +0200

    more coms

--- src/fonctions_reponses.c
+++ src/fonctions_reponses.c
@@ -1,20 +1,19 @@
 #include "fonctions_reponses.h"
 
 void solution1(Tiling* tiles) {
+    // x0, y0 : top left corner
+    // x1, y1 : bottom left corner
     int max = 0;
     int x0_max = 0, y0_max = 0;
     int x1_max = 0, y1_max = 0;
 
     int noBlackFound;
-
-    // x0, y0 is the top left corner
     for (int y0 = 0; y0 < tiles->lines; y0++) {
         for (int x0 = 0; x0 < tiles->columns; x0++) {
-            // x1, y1 are the max values that the reactangle can take
             for (int y1 = y0; y1 < tiles->lines; y1++) {
                 for (int x1 = x0; x1 < tiles->columns; x1++) {
                     noBlackFound = 1;
-                    // ix, iy are the right bottom corner
+                    // ix, iy are the current right bottom corner
                     for (int ix = x0; ix <= x1 && noBlackFound; ix++) {
                         for (int iy = y0; iy <= y1 && noBlackFound; iy++) {
                             if (tiles->values[iy][ix] == 1) noBlackFound = 0;
@@ -50,6 +49,7 @@ void solution1(Tiling* tiles) {
 // Si sa surface est plus grande que tous ceux trouvé auparavant, on le retient comme candidat
 void solution2(Tiling* tiles) {
     // x0, y0 : top left corner
+    // x1, y1 : bottom left corner
     int max = 0;
     int x0_max = 0, y0_max = 0;
     int x1_max = 0, y1_max = 0;
@@ -61,13 +61,14 @@ void solution2(Tiling* tiles) {
             int y1 = y0;
 
             int max_x = tiles->columns - 1;
-
-            // TODO : fix inversion cols/rows
+            //
             while (y1 < tiles->lines && tiles->values[y1][x1] != 1) {
+                // Regardons jusqu'à ou peut aller sur la droite sans tomber sur une case ET sans dépasser la largeur de la dernière itération
                 while (x1 < tiles->columns && tiles->values[y1][x1] != 1 && x1 < max_x) x1++;
                 if (tiles->values[y1][x1] == 1) x1--;  // If the last one was a 1, don't count it
 
-                int area = abs(x1 - x0 + 1) * abs(y1 - y0 + 1);
+                // Comparons l'air de ce rectangle avec l'air du plus grand trouvé
+                int area = (x1 - x0 + 1) * (y1 - y0 + 1);
                 if (area > max) {
                     max = area;
                     x0_max = x0;
@@ -76,8 +77,8 @@ void solution2(Tiling* tiles) {
                     y1_max = y1;
                 }
 
+                // Set the max size in function of the max x found, go on the next line and reset the x1 position : 
                 max_x = x1;
-
                 y1++;
                 x1 = x0;
             }
